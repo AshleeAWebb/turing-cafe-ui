d72028388a9136fec1333998667d3b1fdc9cbd4a
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-05-30 12:14:29 -0400

    Add controlled form that shows when user is typing but does not display as card

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { fetchReservation } from '../apiCalls';
+import Form from '../Form/Form';
 
 class App extends Component {
   constructor() {
@@ -31,7 +32,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+      <Form />
         </div>
         <div className='resy-container'>
   
---
+++ src/Form/Form.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import "../Form/Form.css"
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name:"",
+      date: "",
+      time: "",
+      guest: 0
+    }
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+  
+  render() {
+    const { name, date, time, guest } = this.state;
+  
+    return (
+      <form className="form">
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={this.handleChange}
+          placeholder="Name"
+        />
+        <input
+          type="text"
+          name="date"
+          value={date}
+          onChange={this.handleChange}
+          placeholder="Date (mm/dd)"
+        />
+        <input
+          type="text"
+          name="time"
+          value={time}
+          onChange={this.handleChange}
+          placeholder="Time"
+        />
+        <input
+          type="number"
+          name="guest"
+          value={guest}
+          onChange={this.handleChange}
+          placeholder="Number of guests"
+        />
+        <button type="submit">Make Reservation</button>
+      </form>
+    );
+  }
+}
+export default Form;
