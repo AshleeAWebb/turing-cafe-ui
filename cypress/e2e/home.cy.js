acec04dd603541323f2a862e7d14cf04bfa64952
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-05-30 13:44:41 -0400

    Update the home test to make sure that the intecept is sucessful and the fixture data is render on page

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,26 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Reservations Page', () => {
+  beforeEach(() => {
+    cy.fixture('reservations.json').as('reservationsData');
+    cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', 
+    { fixture: 'reservations.json' });
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('Should display the reservations when user first visits the page', () => {
+    cy.get('@reservationsData').then((reservationsData) => {
+      const firstReservation = reservationsData[0];
+      cy.get('.reservations').children().first()
+        .should('contain', firstReservation.name)
+        .and('contain', firstReservation.date)
+        .and('contain', firstReservation.time)
+        .and('contain', `Number of guests: ${firstReservation.number}`);
+
+      const secondReservation = reservationsData[1];
+      cy.get('.reservations').children().eq(1)
+        .should('contain', secondReservation.name)
+        .and('contain', secondReservation.date)
+        .and('contain', secondReservation.time)
+        .and('contain', `Number of guests: ${secondReservation.number}`);
+    });
+  });
+});
